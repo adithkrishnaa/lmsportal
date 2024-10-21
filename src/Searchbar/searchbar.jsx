19432a6ee4b2b64e0c8de612b8d7e7cb23bf4720
Ankit rai <122200322+Aniraai@users.noreply.github.com>
2024-10-21 23:26:10 +0530

    search functionality done

--- src/Searchbar/searchbar.jsx
+++ src/Searchbar/searchbar.jsx
@@ -1,15 +1,16 @@
 import { IoIosSearch } from "react-icons/io";
 
-const searchbar = () => {
+const Search = ({ onSearch }) => {
   return (
     <div className="sticky z-[5]">
-      <div className="  fixed  right-16  top-24 ">
+      <div className="fixed right-16 top-24">
         <input
           type="text"
           placeholder="Search"
-          className="placeholder-secondary  font-inter border-[1px] border-secondary pl-12 focus:outline-none rounded-3xl px-2 py-2  w-52 xl:w-96  text-black"
+          className="placeholder-secondary font-inter border-[1px] border-secondary pl-12 focus:outline-none rounded-3xl px-2 py-2 w-52 xl:w-96 text-black"
+          onChange={(e) => onSearch(e.target.value)}
         />
-        <div className=" absolute text-secondary top-3 left-5">
+        <div className="absolute text-secondary top-3 left-5">
           <IoIosSearch size={20} />
         </div>
       </div>
@@ -17,4 +18,4 @@ const searchbar = () => {
   );
 };
 
-export default searchbar;
+export default Search;
